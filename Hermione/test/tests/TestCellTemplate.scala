47cacf13ff5562e5373dda98166cb6ff40275bb0
Nicolas PERU <nicolas.peru@gmail.com>
2012-04-28 13:51:18 +0200

    add test for symbol

--- Hermione/test/tests/TestCellTemplate.scala
+++ Hermione/test/tests/TestCellTemplate.scala
@@ -2,7 +2,7 @@ package tests
 
 import org.specs2.mutable.Specification
 import play.api.test.Helpers._
-import models.Cell
+import models._
 
 class TestCellTemplate extends Specification {
   "render cell template" in {
@@ -11,16 +11,22 @@ class TestCellTemplate extends Specification {
     contentAsString(html) must contain("wall-left")
 
     html = views.html.cell(Cell.Empty.withRight(true))
-    contentType(html) must equalTo("text/html")
     contentAsString(html) must contain("wall-right")
 
     html = views.html.cell(Cell.Empty.withTop(true))
-    contentType(html) must equalTo("text/html")
     contentAsString(html) must contain("wall-top")
 
     html = views.html.cell(Cell.Empty.withBottom(true))
-    contentType(html) must equalTo("text/html")
     contentAsString(html) must contain("wall-bottom")
+  }
+
+  "render goal in cell template" in {
+    var html = views.html.cell(Cell.Empty.withGoal(new Goal(Color.Red,Symbol.Star)))
+
+    contentType(html) must equalTo("text/html")
+    contentAsString(html) must contain("symbol")
+    contentAsString(html) must contain("red")
+    contentAsString(html) must contain("star")
   }
 
 }
