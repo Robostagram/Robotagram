c584a12e7b7fedd5bc9ab4431ae96576852c7a9a
Nicolas PERU <nicolas.peru@gmail.com>
2012-04-28 03:35:41 +0200

    et maintenant sans casser la compilation

--- Hermione/app/model/Board.scala
+++ Hermione/app/model/Board.scala
@@ -4,5 +4,5 @@ class Board(w: Int, h: Int) {
   val width: Int = w
   val height: Int = h
 
-  val cells: Array[Array[Cell]] =  Array.fill(height, width){ new Cell() }
+  val cells: Array[Array[Cell]] =  Array.fill(height, width){ new Cell(false, false, null) }
 }
--- Hermione/app/model/DefaultBoard.scala
+++ Hermione/app/model/DefaultBoard.scala
@@ -1,11 +1,6 @@
 package model
 
 
-object DefaultBoard extends Board{
-      def this() = {
-
-           this(16,16);
-           cells(0)(4) = new Cell(true, false, null);
-
-      }
+object DefaultBoard extends Board(16, 16){
+    cells(0)(4) = new Cell(true, false, null)
 }
