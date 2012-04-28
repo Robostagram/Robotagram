1fd61fc3334225bb8f5eaae0d934f3378c11aabf
rrevol <revolromain@gmail.com>
2012-04-28 11:45:22 +0200

    at last it works

--- Hermione/app/models/DefaultBoard.scala
+++ Hermione/app/models/DefaultBoard.scala
@@ -4,18 +4,14 @@ package models
 object DefaultBoard extends Board(16, 16){
 
   // murs sur la gauche
-    for ( i <- 0 to width){
-      cells(0)(i) = cells(0)(i).withEastWall()
+    for ( i <- 0 until width){
+      cells(i)(0) = cells(i)(0).withEastWall()
     }
   // murs en haut
-  for ( j <- 0 to height){
-    cells(j)(0) = cells(j)(0).withNorthWall()
+  for ( j <- 0 until height){
+    cells(0)(j) = cells(0)(j).withNorthWall()
   }
 
   // mapping (coords) -> goal ... pour remplir la board après coup
   var goals = Map((5,2)->new Goal(Color.Red, Symbol.Star))
-
-
-
-
 }
