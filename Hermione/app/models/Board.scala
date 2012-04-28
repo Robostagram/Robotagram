5b5847f8dd03b60c875b731b60d255902dd55b99
markus <markus@atticsweb>
2012-04-28 14:58:34 +0200

    board loading wip

--- Hermione/app/models/Board.scala
+++ Hermione/app/models/Board.scala
@@ -25,33 +25,58 @@ class Board(val width: Int, val height: Int) {
 }
 
 object Board {
+
+  def boardFromFile(path: String): Board = {
+    boardFromString(scala.io.Source.fromFile("file.txt").mkString)
+  }
+
   def boardFromString(stringBoard: String): Board = {
     val lines = stringBoard.split("\\r[\\n]{0,1}")
     val h = lines.length
     val w = lines(0).length
     val board = new Board(w, h)
+    var i = 0
+    var j = 0
     for (line <- lines) {
       for (char <- line.split("")) {
         char match {
-          case "a" => board.cells(w)(h) = new Cell(true, false, false, false, null)
-          case "b" => board.cells(w)(h) = new Cell(false, true, false, false, null)
-          case "c" => board.cells(w)(h) = new Cell(false, false, true, false, null)
-          case "d" => board.cells(w)(h) = new Cell(false, false, false, true, null)
-          case "e" => board.cells(w)(h) = new Cell(true, true, false, false, null)
-          case "f" => board.cells(w)(h) = new Cell(true, false, true, false, null)
-          case "g" => board.cells(w)(h) = new Cell(true, false, false, true, null)
-          case "h" => board.cells(w)(h) = new Cell(false, true, true, false, null)
-          case "i" => board.cells(w)(h) = new Cell(false, true, false, true, null)
-          case "j" => board.cells(w)(h) = new Cell(false, false, true, true, null)
-          case "k" => board.cells(w)(h) = new Cell(true, true, true, false, null)
-          case "l" => board.cells(w)(h) = new Cell(true, true, false, true, null)
-          case "m" => board.cells(w)(h) = new Cell(true, false, true, true, null)
-          case "n" => board.cells(w)(h) = new Cell(false, true, true, true, null)
-          case "o" => board.cells(w)(h) = new Cell(true, true, true, true, null)
+          case "a" => board.cells(i)(j) = updateCell(board, i, j, true, false, false, false)
+          case "b" => board.cells(i)(j) = updateCell(board, i, j, false, true, false, false)
+          case "c" => board.cells(i)(j) = updateCell(board, i, j, false, false, true, false)
+          case "d" => board.cells(i)(j) = updateCell(board, i, j, false, false, false, true)
+          case "e" => board.cells(i)(j) = updateCell(board, i, j, true, true, false, false)
+          case "f" => board.cells(i)(j) = updateCell(board, i, j, true, false, true, false)
+          case "g" => board.cells(i)(j) = updateCell(board, i, j, true, false, false, true)
+          case "h" => board.cells(i)(j) = updateCell(board, i, j, false, true, true, false)
+          case "i" => board.cells(i)(j) = updateCell(board, i, j, false, true, false, true)
+          case "j" => board.cells(i)(j) = updateCell(board, i, j, false, false, true, true)
+          case "k" => board.cells(i)(j) = updateCell(board, i, j, true, true, true, false)
+          case "l" => board.cells(i)(j) = updateCell(board, i, j, true, true, false, true)
+          case "m" => board.cells(i)(j) = updateCell(board, i, j, true, false, true, true)
+          case "n" => board.cells(i)(j) = updateCell(board, i, j, false, true, true, true)
+          case "o" => board.cells(i)(j) = updateCell(board, i, j, true, true, true, true)
           case _ => ()
         }
+        j += 1
       }
+      i += 1
     }
     board
+  }
+
+  def updateCell(board: Board, i: Int, j: Int, top: Boolean, right: Boolean, bottom: Boolean, left: Boolean): Cell = {
+    val cell = new Cell(top, right, bottom, left, null)
+    updateUpAndLeft(board, i, j)
+    cell
+  }
+
+  def updateUpAndLeft(board: Board, i: Int, j: Int) {
+    val newCell = board.cells(i)(j)
+    if (i > 0) {
+      board.cells(i-1)(j) = board.cells(i-1)(j).withBottom()
+    }
+    if(j > 0) {
+      board.cells(i)(j-1) = board.cells(i)(j-1).withRight()
+    }
   }
 }
