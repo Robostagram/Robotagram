09fe6564b8f9160141c49e9826d536d302da4ab6
Nicolas PERU <nicolas.peru@gmail.com>
2012-04-28 12:38:38 +0200

    first test

---
+++ Hermione/test/TestBoard.scala
@@ -0,0 +1,15 @@
+import org.specs2.mutable._
+
+import play.api.test._
+import play.api.test.Helpers._
+
+class TestBoard extends Specification {
+  "respond to the board Action" in {
+    val result = controllers.Application.board(FakeRequest())
+
+    status(result) must equalTo(OK)
+    contentType(result) must beSome("text/html")
+    charset(result) must beSome("utf-8")
+    contentAsString(result) must contain("<table id=\"boardGame\">")
+  }
+}
