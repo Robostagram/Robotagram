55c26adef4cb599b9172427b9a97ab95acf2b11c
Nicolas PERU <nicolas.peru@gmail.com>
2012-04-29 13:47:51 +0200

    correct login with enter key

--- Hermione/public/javascripts/robotagram.js
+++ Hermione/public/javascripts/robotagram.js
@@ -266,7 +266,7 @@
         $("#play").click(loadNewGameFromLogin);
         $("#nickname").keypress(function (e) {
             if (e.keyCode == 13) {
-                loadNewGame();
+                loadNewGameFromLogin();
                 $("#nickModal").modal('hide');
             }
         });
