24bff47b930da970c072b9ed84727d0046f73c55
Thibaud Desodt <tibo.desodt@gmail.com>
2012-05-15 20:53:35 +0200

    - notify on moving/moved - still have to remove some console.debug - score submission is broken :-/

--- public/javascripts/robotagram.js
+++ public/javascripts/robotagram.js
@@ -7,12 +7,19 @@ var EVENT_GAME_TIMEUP = "timeUp.game.robotagram";
 
 function requestSelectedRobotMovement(direction_keyboard_code){
     var color = ROBOT_COLORS[getIndexOfCurrentlySelectedRobot()];
-    var direction = DIRECTIONS[direction_keyboard_code - MAGIC_NUMBER];
+    var direction = directionCodeToString(direction_keyboard_code);
     var $robotToMove = $("td .robot." + color);
     $robotToMove.trigger(REQUEST_ROBOT_MOVE, [direction, color]);
 }
 
+//user friendly version of a direction (Up, Down etc ...)
+function directionCodeToString(direction_keyboard_code){
+    return DIRECTIONS[direction_keyboard_code - MAGIC_NUMBER];
+}
 
+function directionStringToCode(directionName){
+    return MAGIC_NUMBER + (DIRECTIONS.indexOf(directionName)) % 4;
+}
 
 // keyboard handler for robots moves
 function keypressHandler(event) {
@@ -59,19 +66,16 @@ function selectNextRobot() {
     if (curColorIndex >= ROBOT_COLORS.length) {
         curColorIndex = 0;
     }
-
     selectByColor(ROBOT_COLORS[curColorIndex]);
 }
 
 // select the previous robot (order defined by ROBOT_COLORS
 function selectPreviousRobot() {
     var curColorIndex = getIndexOfCurrentlySelectedRobot();
-
     curColorIndex -= 1;
     if (curColorIndex < 0) {
         curColorIndex = ROBOT_COLORS.length - 1;
     }
-
     selectByColor(ROBOT_COLORS[curColorIndex]);
 }
 
@@ -81,14 +85,6 @@ function selectByColor(colorName) {
     $("td .robot." + colorName).toggleClass("selected");
 }
 
-/* called when clicking on the robot */
-function robotClickHandler() {
-    var $this = $(this);
-    if (!$this.is(".selected")) {
-        $(".selected").toggleClass("selected");
-    }
-    $(this).toggleClass("selected");
-}
 
 // direction:
 // 105: i : up
@@ -114,15 +110,13 @@ var REDO = 99;
 var SELECT_PREVIOUS = 115;
 var SELECT_NEXT = 100;
 
-/* global variable for current number of moves ...*/
-//var moves = 0;
+/* stack of moves for undo/redo ...*/
 var moves = new Array();
 var undoIndex = 0;
 
 var moving = false;
 
-// move the robot in the requested direction
-//
+// move the robot of a given color in the requested direction
 function moveRobot(color, direction, keepHistory) {
     var $robot = $("td .robot." + color), originCell, destinationCell = null, previousDestination, nextDestination;
     if ($robot.length == 0) {
@@ -147,7 +141,8 @@ function moveRobot(color, direction, keepHistory) {
         // destination finale du robot
         destinationCell = nextDestination;
         if (originCell !== destinationCell) { // = robot can move in that direction
-
+            // notify people that we start moving - possibly add in the coordinates ...
+            $robot.trigger(EVENT_ROBOT_MOVING, [directionCodeToString(direction), color]);
             // current absolute position ?
             var originalPos = originCell.offset();
             var origTop = originalPos.top;
@@ -170,15 +165,15 @@ function moveRobot(color, direction, keepHistory) {
                            function() {
                                // Animation complete : remettre le robot dans la cellule de destination
                                $(this).css({left:'0px', top:'0px'}).appendTo(destinationCell.children().first()).offset(0, 0);
+                               $(this).trigger(EVENT_ROBOT_MOVED, [directionCodeToString(direction), color]); //notify that we ae done moving ...
                            });
             while(!keepHistory && moves.length > undoIndex) {
                 moves.pop();
             }
-            moves.push({"movement":{"robot":ROBOT_COLORS[getIndexOfCurrentlySelectedRobot()], "originRow":originCell.data("row"), "originColumn":originCell.data("column"), "direction":DIRECTIONS[direction-MAGIC_NUMBER]}});
+            moves.push({"movement":{"robot":color, "originRow":originCell.data("row"), "originColumn":originCell.data("column"), "direction": directionCodeToString(direction)}});
             undoIndex++;
             $("#currentScore").text(undoIndex + "");
             if (hasReachedObjective($robot, destinationCell)) {
-
                 window.onunload = null;// to prevent call to leaveGame() (see registration in javascript in initListeners())
                 $("#winModal").modal('show');
             }
@@ -302,9 +297,18 @@ function nextCell(td, direction) {
     }
 }
 
-function initListeners() {
+
+function setUpGameControlHandlers(){
+
     $(window).keypress(keypressHandler);
-    $("td .robot").click(robotClickHandler);
+
+    $("td .robot").click(function(){
+        var $this = $(this);
+        if (!$this.is(".selected")) {
+            $(".selected").toggleClass("selected");
+        }
+        $(this).toggleClass("selected");
+    });
 
     //on triche, et les touches affichées marchent comme un clavier
     $("#key-up").click(function() {
@@ -319,19 +323,23 @@ function initListeners() {
     $("#key-right").click(function() {
         requestSelectedRobotMovement(DIRECTION_RIGHT);
     });
-	$("#key-next").click(function() {
+    $("#key-next").click(function() {
         selectNextRobot();
     });
-	$("#key-prev").click(function() {
+    $("#key-prev").click(function() {
         selectPreviousRobot();
     });
-	$("#key-undo").click(function() {
+    $("#key-undo").click(function() {
         undo();
     });
-	$("#key-redo").click(function() {
+    $("#key-redo").click(function() {
         redo();
     });
 
+}
+
+
+function setUpHeaderShortcuts(){
     // robot from header selects the robot on the board
     $("a#headerRobot").click(function (e) {
         $(".selected").removeClass("selected");
@@ -341,13 +349,13 @@ function initListeners() {
 
     // make the objective more obvious when hovering in the header
     $("a#headerGoal span.symbol").click(function(){
-            $("div.symbol:not(#objective)").toggleClass("transparent");
+        $("div.symbol:not(#objective)").toggleClass("transparent");
     });
 
+}
 
+function setUpHelpAndTooltips(){
     var $robotOfObjective = $("#robotForObjective");
-    //select the robot correspounding to the objective on page load
-    $robotOfObjective.addClass("selected");
 
     $robotOfObjective.tooltip({
         title:"Bring this robot ...",
@@ -382,7 +390,18 @@ function initListeners() {
     // tooltip on the objective a bit after the robot
     setTimeout(function(){$objective.tooltip('show').effect('pulsate', { times:3 } , 400);}, 1500);
     setTimeout(function(){$objective.tooltip('hide');}, 3000);
+}
 
+function initListeners(){
+    setUpGameControlHandlers();
+
+    setUpHeaderShortcuts();
+
+    setUpHelpAndTooltips();
+
+    var $robotOfObjective = $("#robotForObjective");
+    //select the robot correspounding to the objective on page load
+    $robotOfObjective.addClass("selected");
 
     connectPlayer();
 
@@ -405,15 +424,23 @@ function initListeners() {
         moveRobot(color, MAGIC_NUMBER + (DIRECTIONS.indexOf(direction)) % 4);//keepHistory
     });
 
+    $(window).on(EVENT_ROBOT_MOVING, function(e, direction, color){
+        console.debug(e.type, e.namespace, direction, color);
+    });
+    $(window).on(EVENT_ROBOT_MOVED, function(e, direction, color){
+        console.debug(e.type, e.namespace, direction, color);
+    });
+
+    // do it only once
+    $(window).one(EVENT_GAME_TIMEUP, function(e){
+        console.debug(e.type, e.namespace);
+        $("#endOfGameModal").modal('show');
+    });
 }
 
 // function called when end of game is reached
-var endOfGameNotified = false;
 function notifyEndOfGame(){
-    if(!endOfGameNotified){
-        endOfGameNotified = true;
-        $("#endOfGameModal").modal('show');
-    }
+    $(window).trigger(EVENT_GAME_TIMEUP);
 }
 
 // store the actual time left (as double, with details and stuff )
@@ -513,7 +540,12 @@ function reSyncGameStatusWithServer() {
 
     function sendScore() {
         // how to handle submission of a game that is finished ?
-        var message = JSON.stringify({"solution":{"player":$("#userName").text(), "moves":moves.slice(0, undoIndex)}});
+        // the server expects moves to be a list of strings, instead of a list of proper objects ...
+        // jsonify it before, but this can be improved
+        var movesToSend = moves.slice(0, undoIndex).map(function(){return JSON.stringify(this)});
+        var message = JSON.stringify({"solution":{"player":$("#userName").text(), "moves":movesToSend}});
+        console.debug(message);
+        console.debug(movesToSend);
         gameSocket.send(message);
     }
 
