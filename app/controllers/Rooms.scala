94414a3a99b97c17e64b14ae7e37786841363fe6
Thibaud Desodt <tibo.desodt@gmail.com>
2012-07-12 08:41:30 +0200

    issue #25 display a preview of a game (with robots and goals !)

--- app/controllers/Rooms.scala
+++ app/controllers/Rooms.scala
@@ -1,7 +1,7 @@
 package controllers
 
 import play.api.mvc._
-import models.{DbRoom, DbGame, User, DbBoard}
+import models._
 
 // Room controller
 object Rooms extends Controller{
@@ -10,6 +10,16 @@ object Rooms extends Controller{
   def gamesByRoom(roomName:String) = Action { implicit request =>
     DbRoom.findByName(roomName).map{dbRoom =>
       Ok(views.html.rooms.gamesByRoom(dbRoom, DbGame.getLatestGamesByRoom(dbRoom.id.get, limit = 15), User.fromRequest))
+    }
+    .getOrElse(NotFound)
+  }
+
+  def previewGame(roomName: String, gameId : String) = Action {implicit request =>
+    DbRoom.findByName(roomName).map {dbRoom =>
+      Game.load(dbRoom.name, gameId).map{g =>
+        Ok(views.html.rooms.previewGame(dbRoom, g, User.fromRequest))
+      }
+      .getOrElse(NotFound)
     }
     .getOrElse(NotFound)
 
