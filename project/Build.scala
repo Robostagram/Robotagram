9ee9ec24fe39e623eab531b4e2825b9c72612494
Thibaud Desodt <tibo.desodt@gmail.com>
2012-07-19 08:17:58 +0200

    issue #9 use less css version of bootstrap
    * putting all the less files in assets (but served from the /public route)
    * automatically generates a minified and a not minified version
    * had to tweak a bit Build.scala so it takes the correct entry point for bootstrap less structure
    .... (minified in prod, not minified in dev)

--- project/Build.scala
+++ project/Build.scala
@@ -11,8 +11,18 @@ object ApplicationBuild extends Build {
       "postgresql" % "postgresql" % "9.1-901-1.jdbc4"
     )
 
+  // taken from : https://github.com/playframework/Play20/wiki/Tips
+  // Only compile the bootstrap bootstrap.less file and any other *.less file in the stylesheets directory
+    def customLessEntryPoints(base: File): PathFinder = (
+      (base / "app" / "assets" / "stylesheets" / "bootstrap" * "bootstrap.less") +++
+      (base / "app" / "assets" / "stylesheets" / "bootstrap" * "responsive.less") +++
+      (base / "app" / "assets" / "stylesheets" * "*.less")
+    )
+
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
-      // Add your own project settings here      
+      // Add your own project settings here
+      // compile bootstrap.less, the entry point for Bootstrap LESS CSS
+      lessEntryPoints <<= baseDirectory(customLessEntryPoints)
     )
 
 }
