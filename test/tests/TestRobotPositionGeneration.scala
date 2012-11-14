71e4bbd0b785fdd551aa5b3cbe5df5900f5396d1
darkus <markus.galileo@gmail.com>
2012-11-14 17:20:16 +0100

    obsolete methods removed, test still needs review for usefulness

--- test/tests/TestRobotPositionGeneration.scala
+++ test/tests/TestRobotPositionGeneration.scala
@@ -2,37 +2,17 @@ package tests
 
 import org.specs2.mutable.Specification
 import models._
+import models.Color._
 import collection.immutable.HashSet
+import collection.immutable.HashMap
+import java.util.Date
 
-object gameMockMiddlePosition extends Game(DefaultBoard,Goal.randomGoal(), 120){
-  var call:Int = 0
-  val returns = Array(7,7,8,8,7,8,8,7,1,2,3,4,5,6,9,10,11,12);
-  override def getRandomPosition(max:Int)={
-    var result = 12
-    if(call<returns.size){
-      result = returns(call)
-    }
-    call+=1
-    result
-  }
-}
-object gameMockRobotOverlap extends Game(DefaultBoard,Goal.randomGoal(), 120){
-  var call:Int = 0
-  val returns = Array(12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,1,2,3,4,5,6,9,10,11,12);
-  override def getRandomPosition(max:Int)={
-    var result = 13
-    if(call<returns.size){
-      result = returns(call)
-    }
-    call+=1
-    result
-  }
-}
+object MockGame extends Game("mockmiddle", DefaultBoard, Goal.randomGoal, new Date, new Date, new HashMap[Color, Robot]){}
 
 class TestRobotPositionGeneration extends Specification{
 
    "generate random robots list  produce non center postion" in {
-      for(robot <- gameMockMiddlePosition.robots.values){
+      for(robot <- MockGame.robots.values){
          if(robot.posX==7){ robot.posY must_!= 7}
          if(robot.posX==7){ robot.posY must_!= 8}
          if(robot.posX==8){ robot.posY must_!= 7}
@@ -42,7 +22,7 @@ class TestRobotPositionGeneration extends Specification{
 
     "produce unique position for each robot" in {
       var setPos:Set[Tuple2[Int,Int]] = new HashSet[Tuple2[Int,Int]];
-      for(robot <- gameMockRobotOverlap.robots.values){
+      for(robot <- MockGame.robots.values){
         setPos.contains((robot.posX,robot.posY)) must beFalse
         setPos+=((robot.posX,robot.posY))
       }
