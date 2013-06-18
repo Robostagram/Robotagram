72e2b5f4b9d998259a84f3f374d1203063c1c257
unknown <revolromain@gmail.com>
2013-06-18 17:58:16 +0200

    override a few templates, some are still missing

--- app/controllers/SecureSocialTemplatesPlugin.scala
+++ app/controllers/SecureSocialTemplatesPlugin.scala
@@ -1,19 +1,22 @@
 package controllers
 
-import play.api.mvc.Request
+import play.api.mvc.{RequestHeader, Request}
 import play.api.data.Form
-import play.api.templates.Html
+import play.api.templates.{Txt, Html}
+import securesocial.controllers.Registration.RegistrationInfo
 
 class SecureSocialTemplatesPlugin(application: play.api.Application) extends securesocial.controllers.DefaultTemplatesPlugin(application) with CookieLang {
 
-  /**
-   * Returns the html for the login page
-   * @param request
-   * @tparam A
-   * @return
-   */
   override def getLoginPage[A](implicit request: Request[A], form: Form[(String, String)],
                                msg: Option[String] = None): Html = {
     views.html.authentication.login(form, msg)(request.flash, request, lang)
+  }
+
+  override def getStartSignUpPage[A](implicit request: Request[A], form: Form[String]): Html = {
+    views.html.authentication.startSignUp(form)(request.flash, request, lang)
+  }
+
+  override def getSignUpPage[A](implicit request: Request[A], form: Form[RegistrationInfo], token: String): Html = {
+    views.html.authentication.signUp(form, token)(request.flash, request, lang)
   }
 }
